1a5c4a3028c18638802137b107f9ee7a839407c6
Reeshul <reeshul@hotmail.com>
2021-01-15 23:25:34 +0000

    updated tests to follow guidance for the purposes of this exercise

--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -17,37 +17,59 @@ require_relative '../lib/secret_diary'
 # as expected.
 ### WARNING ###
 
-RSpec.describe SecretDiary do
+require_relative '../lib/secret_diary'
 
-  let(:read_locked_diary) { double :read => "reading my secret story" }
-  let(:write_in_locked_diary) { double :write => "writing my secret story" }
+### WARNING ###
+# For the purposes of this exercise, you are testing after
+# the code has been written. This means your tests are prone
+# to false positives!
+#
+# Make sure your tests work by deleting the bodies of the
+# methods in `secret_diary.rb`, like this:
+#
+# ```ruby
+# def write(new_message)
+# end
+# ```
+#
+# If they fail, then you know your tests are working
+# as expected.
+### WARNING ###
+
+RSpec.describe SecretDiary do
 
   context "when locked" do 
     it "cannot be read" do
-      diary = SecretDiary.new(read_locked_diary)
-      expect(diary.read).to eq("Go away!")
+      read_locked_diary = double
+      expect(read_locked_diary).not_to receive(:read)
+      secret_diary = SecretDiary.new(read_locked_diary)
+      secret_diary.read
     end
 
     it "cannot be written in" do
-      diary = SecretDiary.new(write_in_locked_diary)
-      expect(diary.write("writing my secret story")).to eq("Go away!")
+      write_in_locked_diary = double
+      expect(write_in_locked_diary).not_to receive(:write)
+      secret_diary = SecretDiary.new(write_in_locked_diary)
+      secret_diary.lock
+      secret_diary.write("writing my secret story")
     end
   end
-
-  let(:read_unlocked_diary) { double :read => "reading my secret story" }
-  let(:write_in_unlocked_diary) { double :write => "writing my secret story" }
-
+  
   context "when unlocked" do
     it "can get read" do
-      diary = SecretDiary.new(read_unlocked_diary)
-      diary.unlock
-      expect(diary.read).to eq("reading my secret story")
+      read_unlocked_diary = double
+      expect(read_unlocked_diary).to receive(:read)
+      secret_diary = SecretDiary.new(read_unlocked_diary)
+      secret_diary.unlock
+      secret_diary.read
     end
 
     it "can get written in" do
-      diary = SecretDiary.new(write_in_unlocked_diary)
-      diary.unlock
-      expect(diary.write("writing my secret story")).to eq(nil)
+      write_in_unlocked_diary = double
+      expect(write_in_unlocked_diary).to receive(:write)
+      secret_diary = SecretDiary.new(write_in_unlocked_diary)
+      secret_diary.unlock
+      secret_diary.write("writing my secret story")
     end
   end
 end
